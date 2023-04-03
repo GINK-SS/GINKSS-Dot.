378fd4acdc447da3a1f2cd08abce20b13a89e420
Sangmin Lee <gink.ss00@gmail.com>
2023-04-04 03:05:25 +0900

    chore: 초반 라이브러리 설치 및 설정
    설치:
    - react-route-dom v6
    - styled-components
    - styled-reset
    - react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <ThemeProvider theme={lightTheme}>
+      <HelmetProvider>
+        <GlobalStyle />
+        <App />
+      </HelmetProvider>
+    </ThemeProvider>
+  </BrowserRouter>
+);
