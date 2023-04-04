9c25c1661d162c56a8ffeafffba629a839756e6b
Sangmin Lee <gink.ss00@gmail.com>
2023-04-05 02:54:18 +0900

    feat: [메인 페이지] 스크롤 안되게 구현
    메인 페이지는 스크롤이 필요 없고, 스크롤 되는 것을 막기 위해 기능을 구현하였음

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import Background from '../components/main/Background';
 
 const MainPage = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
     <Background>
     </Background>
