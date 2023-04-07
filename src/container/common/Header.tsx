352ab912850e5d500e7de12a7d9550e6766f1dd9
Sangmin Lee <gink.ss00@gmail.com>
2023-04-08 03:09:55 +0900

    feat: [헤더] 로고 누르면 메인 페이지 이동 구현

--- src/container/common/Header.tsx
+++ src/container/common/Header.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '../../components/header/Button';
 import Wrapper from '../../components/header/Wrapper';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const [isHover, setIsHover] = useState(false);
+
   const handleMouseOver = () => setIsHover(true);
   const handleMouseOut = () => setIsHover(false);
+
+  const onLogo = () => {
+    if (pathname !== '/') navigate('/');
+  };
 
   return (
     <Wrapper>
       <Button
         icon={isHover ? 'logoHover' : 'logo'}
-        handleClick={() => {}}
+        handleClick={onLogo}
         handleMouseOver={handleMouseOver}
         handleMouseOut={handleMouseOut}
       />
