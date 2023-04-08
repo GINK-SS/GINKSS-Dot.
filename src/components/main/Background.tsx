d6172e5481f7e3b7ba28e268b1e6f27bef29b553
Sangmin Lee <gink.ss00@gmail.com>
2023-04-09 00:39:23 +0900

    feat: 다크모드 여부에 따라 백그라운드 변경 구현

--- src/components/main/Background.tsx
+++ src/components/main/Background.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from 'react';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import darkBackground from '../../assets/darkMainBackground.png';
 import background from '../../assets/mainBackground.png';
+import { themeState } from '../../store/theme';
 
 interface BackgroundProps {
   children: ReactNode;
@@ -8,14 +11,16 @@ interface BackgroundProps {
 
 /** 메인 배경화면 */
 const Background = ({ children }: BackgroundProps) => {
-  return <Container>{children}</Container>;
+  const isDark = useRecoilValue(themeState);
+
+  return <Container isDark={isDark}>{children}</Container>;
 };
 
 export default Background;
 
-const Container = styled.div`
+const Container = styled.div<{ isDark: boolean }>`
   position: relative;
   height: 100vh;
   overflow: hidden;
-  background: url(${background});
+  background: url(${({ isDark }) => (isDark ? darkBackground : background)});
 `;
