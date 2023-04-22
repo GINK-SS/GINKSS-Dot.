008c4154a375b91d7633cf96a8dc2ba3ec82f05f
Sangmin Lee <gink.ss00@gmail.com>
2023-04-23 04:30:01 +0900

    feat: [컨텍] EmailJS 사용하여 이메일 전송 기능 구현

--- src/container/modal/Contact.tsx
+++ src/container/modal/Contact.tsx
@@ -1,7 +1,39 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Input from '../../components/modal/Input';
+import Wrapper from '../../components/modal/Wrapper';
+import Title from '../../components/modal/Title';
+import TextArea from '../../components/modal/TextArea';
+import SubmitBtn from '../../components/modal/SubmitBtn';
+
 const Contact = () => {
+  const formRef = useRef(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formRef.current !== null) {
+      emailjs.sendForm(
+        process.env.REACT_APP_SERVICE_ID!,
+        process.env.REACT_APP_TEMPLATE_ID!,
+        formRef.current,
+        process.env.REACT_APP_PUBLIC_KEY!
+      );
+    }
+  };
+
   return (
-    <div>
-    </div>
+    <Wrapper>
+      <Title />
+      <Input title="보내는 곳" isdisabled />
+      <form ref={formRef} onSubmit={sendEmail}>
+        <Input title="제목" name="title" />
+        <Input title="이름" name="name" />
+        <Input title="메일 주소" name="email" />
+        <TextArea name="message" />
+        <SubmitBtn />
+      </form>
+    </Wrapper>
   );
 };
 
