78414acbabfe77495df2f51e14996ca4da58765b
Sangmin Lee <gink.ss00@gmail.com>
2023-04-23 21:48:06 +0900

    feat: [컨텍] 메일 전송 시, 로딩 스피너 띄우기 구현 및 일부 코드 수정

--- src/container/modal/Contact.tsx
+++ src/container/modal/Contact.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Input from '../../components/modal/Input';
 import Wrapper from '../../components/modal/Wrapper';
@@ -7,21 +7,28 @@ import TextArea from '../../components/modal/TextArea';
 import SubmitBtn from '../../components/modal/SubmitBtn';
 import { useSetRecoilState } from 'recoil';
 import { contactState } from '../../store/modal';
+import Spinner from '../../components/common/Spinner';
 
 const Contact = () => {
   const formRef = useRef(null);
   const setIsContact = useSetRecoilState(contactState);
+  const [isSubmiting, setIsSubmiting] = useState(false);
 
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsSubmiting(true);
 
     if (formRef.current !== null) {
-      emailjs.sendForm(
+      const { status } = await emailjs.sendForm(
         process.env.REACT_APP_SERVICE_ID!,
         process.env.REACT_APP_TEMPLATE_ID!,
         formRef.current,
         process.env.REACT_APP_PUBLIC_KEY!
       );
+
+      if (status === 200) closePopup();
+
+      setIsSubmiting(false);
     }
   };
 
@@ -32,17 +39,20 @@ const Contact = () => {
   const closePopup = () => setIsContact(false);
 
   return (
-    <Wrapper onClick={handleOuterClick}>
-      <Title />
-      <Input title="보내는 곳" isdisabled />
-      <form ref={formRef} onSubmit={sendEmail}>
-        <Input title="제목" name="title" />
-        <Input title="이름" name="name" />
-        <Input title="메일 주소" name="email" />
-        <TextArea name="message" />
-        <SubmitBtn />
-      </form>
-    </Wrapper>
+    <>
+      <Spinner isActive={isSubmiting} />
+      <Wrapper onClick={handleOuterClick}>
+        <Title />
+        <Input title="보내는 곳" isdisabled />
+        <form ref={formRef} onSubmit={sendEmail}>
+          <Input title="제목" name="title" />
+          <Input title="이름" name="name" />
+          <Input title="메일 주소" name="email" />
+          <TextArea name="message" />
+          <SubmitBtn />
+        </form>
+      </Wrapper>
+    </>
   );
 };
 
