86ab48412e5308df913afe607c00f45e404fb1d2
Sangmin Lee <gink.ss00@gmail.com>
2023-05-01 10:17:34 +0900

    feat: 전체 배경화면 변경

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import './fonts/font.css';
+import hideout from '../assets/hideout.svg';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-  body, a {
+  body {
     color: ${({ theme }) => theme.textColor};
     font-size: 16px;
     font-family: 'Noto Sans KR', sans-serif;
     background-color: ${({ theme }) => theme.bgColor};
-    transition: ${({ theme }) => theme.transDuration};
+    background-image: url(${hideout});
+    background-size: 70px 70px;
   }
   a {
     color: ${({ theme }) => theme.textColor};
